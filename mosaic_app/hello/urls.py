766e0c40f2eec42fbcdf6e994b922b8394afd4f3
agematsu <agematsu@ymgsapo.com>
2018-11-05 23:59:45 +0900

    画像アップロード、表示を追加

--- mosaic_app/hello/urls.py
+++ mosaic_app/hello/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('upload', views.model_form_upload, name='upload')
 ]
--- mosaic_app/hello/views.py
+++ mosaic_app/hello/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .forms import DocumentForm
+from .models import Document
 
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("Hello Django!!!")
+    data = Document.objects.all()
+    params = {'data': data}
+    return render(request, 'index.html', params)
+
+
+def model_form_upload(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = DocumentForm()
+    return render(request, 'model_form_upload.html', {
+        'form': form
+    })
